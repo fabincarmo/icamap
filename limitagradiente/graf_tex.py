668358084bb5d9e0bfa989658d37bf22c241f53c
fabincarmo <fabincarmo@gmail.com>
2014-02-20 15:29:49 -0300

    1.1

--- limitagradiente/graf_tex.py
+++ limitagradiente/graf_tex.py
@@ -10,14 +10,3 @@ def salvagraf(arquivo):
     savefig(arquivo,bbox_inches="tight")
     return 
 
-
-#from numpy import linspace, pi, sin, arange, size
-##edit: with an example of marking an arbitrary subset of points, as requested in the comments:
-##
-#xs = linspace(-pi, pi, 100)
-#ys = sin(xs)
-#markers_on = arange(0,size(xs),10)
-#plot(xs, ys, 'g-')
-#plot(xs[markers_on], ys[markers_on], 'r', label="Teste")
-#legend(loc=4)
-#show()
